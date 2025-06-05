2e6c041228cec9ceff24a8d060e1c8750e16375c
CjsCommissionsRoblox <cjscommisions@outlook.com>
2025-06-05 23:11:41 +0100

    Update app.py
    Signed-off-by: CjsCommissionsRoblox <cjscommisions@outlook.com>

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
+__version__ = "1.1.2"
+
 import html
 import os
 
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
-from sanic import Sanic
-from sanic.response import html as sanic_html
+from sanic import Sanic, response
 from sanic.exceptions import NotFound
 from jinja2 import Environment, FileSystemLoader
 
 from core.models import LogEntry
 
-load_dotenv()
 
-app = Sanic("LogViewerApp")
-
+app = Sanic(__name__)
 app.static("/static", "./static")
 
 jinja_env = Environment(loader=FileSystemLoader("templates"))
@@ -21,11 +20,13 @@ jinja_env = Environment(loader=FileSystemLoader("templates"))
 
 def render_template(name, *args, **kwargs):
     template = jinja_env.get_template(name + ".html")
-    return sanic_html(template.render(*args, **kwargs))
+    return response.html(template.render(*args, **kwargs))
 
+
+app.ctx.render_template = render_template
 
 @app.exception(NotFound)
-async def not_found_handler(request, exception):
+async def not_found(request, exc):
     return render_template("not_found")
 
 
@@ -33,10 +34,9 @@ async def not_found_handler(request, exception):
 async def index(request):
     return render_template("not_found")
 
-
 if __name__ == "__main__":
     app.run(
         host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", 8096)),
-        debug=os.getenv("DEBUG", "false").lower() == "true"
+        port=os.getenv("PORT", 8096),
+        debug=bool(os.getenv("DEBUG", False)),
     )
